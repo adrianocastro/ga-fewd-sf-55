72f72a638fc81a584bb01fb5ab4e770147ee5b28
Adriano Castro <ad@adrianocastro.net>
2017-05-09 15:52:39 -0700

    Added homework for lesson 11.

---
+++ lessons/11/assignment/README.md
@@ -0,0 +1,31 @@
+# FEWD Assignment #9: Ice-Cream Maker
+
+## Description
+
+Using the [starter code](starter_code/) provided, implement the Ice Cream ordering system [demoed in the solution file](solution/index.html).
+
+---
+
+## Real-World Applications
+
+- Use jQuery to query the HTML/DOM tree
+- Use CSS to style pages
+- Keep the behavior (JS) and presentation (CSS) layers separate
+
+---
+
+## Technical Requirements 
+
+- Use jQuery hosted locally
+- Use advanced selectors to select and style elements on the page
+- Keep the JavaScript and CSS separate by making the layout changes using CSS and leveraging classes/IDs instead of directly manipulating the styles using JavaScript
+
+---
+
+## Evaluation / Submission
+
+Please submit your work via your class GitHub repository.
+
+The file should be inside the `assignments/11-ice-cream` folder.
+
+The instructional team will grade each technical requirement and provide a numeric grade on a scale: does not meet expectations (0); meets expectations (1); exceeds expectations (2).
---
+++ lessons/11/assignment/ice-cream/starter_code/scripts/index.js
@@ -0,0 +1,32 @@
+// Define a function makeCone.
+// It should accept two parameters, flavor and numberScoops.
+
+// The function should take the parameters and log the following to the console:
+// "Coming right up! One {flavor} ice cream cone with {numberScoops} scoops."
+
+// Hint: You'll want to use string concatenation (+) to combine the variables
+// with the human-readable text when logging the message to the console.
+
+// Now, call that function, passing in "chocolate" and 3 as the arguments.
+
+// You should see the following messages:
+
+// "Coming right up!"
+// "Flavor: chocolate"
+// "Scoops: 3"
+
+console.log('Get ready for some ice-cream!');
+
+// makeCone function definition
+var makeCone = function (flavor, numberScoops) {
+  console.log('Coming right up!');
+  console.log('Flavor: ' + flavor);
+  console.log('Scoops: ' + numberScoops);
+}
+
+// makeCone function call
+// passing arguments 'chocolate' and 3
+// that correspond to the `flavor` and `numberScoops`
+makeCone('chocolate', 3);
+
+makeCone('vanilla', 1);
